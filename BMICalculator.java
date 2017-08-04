4c83f35e7dadf3486544443b67cac9b82255b343
Alexis Low <alexisthelow@gmail.com>
2017-08-04 14:25:07 -0600

    study drill changes

--- BMICalculator.java
+++ BMICalculator.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class BMICalculator {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		double m, kg, bmi;
+		double ft, in, lbs, m, kg, bmi;
 		
-		System.out.print("Your height in meters: ");
-		m = keyboard.nextDouble();
+		System.out.print("Your height (feet only): ");
+		ft = keyboard.nextDouble();
 		
-		System.out.print("Your weight in kilograms: ");
-		kg = keyboard.nextDouble();
+		System.out.print("Your height (inches only): ");
+		in = (keyboard.nextDouble() + (ft * 12));
+		
+		System.out.print("Your weight in pounds: ");
+		lbs = keyboard.nextDouble();
+		
+		m = (in * 0.0254);
+		kg = (lbs * 0.4535);
 		
 		bmi = kg / (m*m);
 		
