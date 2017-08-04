0215d08190519100885d5af6505e56eb10128da4
Alexis Low <alexisthelow@gmail.com>
2017-08-04 14:19:58 -0600

    first commit

---
+++ BMICalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double m, kg, bmi;
+		
+		System.out.print("Your height in meters: ");
+		m = keyboard.nextDouble();
+		
+		System.out.print("Your weight in kilograms: ");
+		kg = keyboard.nextDouble();
+		
+		bmi = kg / (m*m);
+		
+		System.out.println("Your BMI is " + bmi + ", and you are perfect.");		
+	}
+}
